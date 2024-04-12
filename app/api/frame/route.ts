16ce8924a654bf7f435ccdd2f937e0cf351c4646
Wayne <wytham@simulcore.com>
2024-04-12 13:11:07 +0800

    chore: initial commit

--- app/api/frame/route.ts
+++ app/api/frame/route.ts
@@ -1,47 +1,53 @@
 import { FrameRequest, getFrameMessage, getFrameHtmlResponse } from '@coinbase/onchainkit/frame';
 import { NextRequest, NextResponse } from 'next/server';
 import { NEXT_PUBLIC_URL } from '../../config';
-import usePersistentStore from '../../../store/usePersistentStore';
 
-async function getResponse(req) {
-  const body = await req.json();
-  const { isValid, message } = 
-    process.env.NODE_ENV === 'development' 
-      ? { isValid: true, message: {} }
-      : await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
+async function getResponse(req: NextRequest): Promise<NextResponse> {
+  const body: FrameRequest = await req.json();
+  const { isValid, message } = await getFrameMessage(body, { neynarApiKey: 'NEYNAR_ONCHAIN_KIT' });
 
   if (!isValid) {
     return new NextResponse('Message not valid', { status: 500 });
   }
 
-  usePersistentStore.getState().incrementWaves();
-
+  const text = message.input || '';
   let state = {
     page: 0,
-    waves: usePersistentStore.getState().waves
   };
   try {
-    state = {
-      ...state, 
-      ...JSON.parse(decodeURIComponent(message.state?.serialized ?? '{}'))};
+    state = JSON.parse(decodeURIComponent(message.state?.serialized));
   } catch (e) {
     console.error(e);
+  }
+
+  /**
+   * Use this code to redirect to a different page
+   */
+  if (message?.button === 3) {
+    return NextResponse.redirect(
+      'https://www.google.com/search?q=cute+dog+pictures&tbm=isch&source=lnms',
+      { status: 302 },
+    );
   }
 
   return new NextResponse(
     getFrameHtmlResponse({
       buttons: [
         {
-          label: `Waves: ${state?.waves || 0}`,
+          label: `State: ${state?.page || 0}`,
         },
         {
           action: 'link',
           label: 'OnchainKit',
           target: 'https://onchainkit.xyz',
-        }
+        },
+        {
+          action: 'post_redirect',
+          label: 'Dog pictures',
+        },
       ],
       image: {
-        src: `${NEXT_PUBLIC_URL}/mascot-1.png`,
+        src: `${NEXT_PUBLIC_URL}/park-1.png`,
       },
       postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
       state: {
@@ -52,7 +58,7 @@ async function getResponse(req) {
   );
 }
 
-export async function POST(req) {
+export async function POST(req: NextRequest): Promise<Response> {
   return getResponse(req);
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -5,12 +5,21 @@ import { NEXT_PUBLIC_URL } from './config';
 const frameMetadata = getFrameMetadata({
   buttons: [
     {
-      label: '👋 Wave to Elly!',
+      label: 'Story time',
+    },
+    {
+      action: 'tx',
+      label: 'Send Base Sepolia',
+      target: `${NEXT_PUBLIC_URL}/api/tx`,
+      postUrl: `${NEXT_PUBLIC_URL}/api/tx-success`,
     },
   ],
   image: {
-    src: `${NEXT_PUBLIC_URL}/mascot-1.png`,
-    aspectRatio: '1.91:1',
+    src: `${NEXT_PUBLIC_URL}/park-3.png`,
+    aspectRatio: '1:1',
+  },
+  input: {
+    text: 'Tell me a story',
   },
   postUrl: `${NEXT_PUBLIC_URL}/api/frame`,
 });
@@ -21,7 +30,7 @@ export const metadata: Metadata = {
   openGraph: {
     title: 'zizzamia.xyz',
     description: 'LFG',
-    images: [`${NEXT_PUBLIC_URL}/mascot-1.png`],
+    images: [`${NEXT_PUBLIC_URL}/park-1.png`],
   },
   other: {
     ...frameMetadata,
