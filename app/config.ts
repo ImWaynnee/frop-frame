fbecbb321a6df8977814fafd8d640340c0219b16
Wayne <wytham@simulcore.com>
2024-04-12 16:11:54 +0800

    refactor: coffee addr

--- app/config.ts
+++ app/config.ts
@@ -1,4 +1,4 @@
 // use NODE_ENV to not have to change config based on where it's deployed
 export const NEXT_PUBLIC_URL =
   process.env.NODE_ENV == 'development' ? 'http://localhost:3000' : 'https://frop-frame.vercel.app';
-export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xcD3D5E4E498BAb2e0832257569c3Fd4AE439dD6f';
+export const BUY_MY_COFFEE_CONTRACT_ADDR = '0xdB689Dd8B4B5f9767492b0Bee718D97F875718d0';
